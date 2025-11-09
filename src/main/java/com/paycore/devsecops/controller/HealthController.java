de8bf0d9ae039ab41b43f838a8e6b5c783c8ecff
Agustin Grigaliunas <agustingrigaliunas@gmail.com>
2025-11-09 13:33:54 -0300

    feat: added health stuff

--- src/main/java/com/paycore/devsecops/controller/HealthController.java
+++ src/main/java/com/paycore/devsecops/controller/HealthController.java
@@ -8,7 +8,13 @@ public class HealthController {
 
     @GetMapping("/")
     public String health() {
-        return "OK - PayCore";
+        return "<html><body>" +
+                "<h1>PayCore</h1>" +
+                "<a href=\"/api/payments/preview?description=test\">Preview</a>" +
+                "<a href=\"/api/payments/redirect?url=http://example.com\">Redirect</a>" +
+                "<a href=\"/api/payments/debug?error=fail\">Debug</a>" +
+                "</body></html>";
     }
+
 }
 
