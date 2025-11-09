4eb0349385e8ee1f2ed3017ea713361678134240
Agustin Grigaliunas <agustingrigaliunas@gmail.com>
2025-11-09 13:11:06 -0300

    feat: changed ci dast config

---
+++ src/main/java/com/paycore/devsecops/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.paycore.devsecops.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "OK - PayCore";
+    }
+}
+
